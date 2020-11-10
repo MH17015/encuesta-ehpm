afe479167cd452f8cee778f674b557537c142de4
MH17015 <MH17015@ues.edu.sv>
2020-11-09 18:19:44 -0600

    Modificación 03

--- src/main/java/sv/gob/models/PersonaEjemplo.java
+++ src/main/java/sv/gob/models/PersonaEjemplo.java
@@ -1,8 +1,7 @@
 package sv.gob.models;
 
 public class PersonaEjemplo {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
     private String nombre;
     private String apellido;
@@ -12,23 +11,6 @@ public class PersonaEjemplo {
     	return nombre;	
     }   
 
-=======
->>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
-=======
->>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
-	private String nombre;
-	private String apellido;
-	private String carrera;
-	public String getNombre() {
-		return nombre;
-	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
-=======
->>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
@@ -36,17 +18,7 @@ public class PersonaEjemplo {
 		return apellido;
 	}
 	public void setApellido(String apellido) {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-         this.apellido=apellido;
-
-=======
-		this.apellido = apellido;
->>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
-=======
-		this.apellido = apellido;
->>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
 	}
 	public String getCarrera() {
 		return carrera;
@@ -54,14 +26,7 @@ public class PersonaEjemplo {
 	public void setCarrera(String carrera) {
 		this.carrera = carrera;
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	
->>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
-=======
-	
->>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
+
 	@Override
 	public String toString() {
 		return "PersonaEjemplo [nombre=" + nombre + ", apellido=" + apellido + ", carrera=" + carrera + "]";
