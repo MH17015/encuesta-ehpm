929f7c1ed30d22547c80a4e21f7cfae92dc024c3
MH17015 <MH17015@ues.edu.sv>
2020-11-09 18:16:10 -0600

    Modificación 01

--- src/main/java/sv/gob/models/PersonaEjemplo.java
+++ src/main/java/sv/gob/models/PersonaEjemplo.java
@@ -1,6 +1,7 @@
 package sv.gob.models;
 
 public class PersonaEjemplo {
+<<<<<<< HEAD
 
     private String nombre;
     private String apellido;
@@ -10,13 +11,18 @@ public class PersonaEjemplo {
     	return nombre;	
     }   
 
+=======
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
 	private String nombre;
 	private String apellido;
 	private String carrera;
 	public String getNombre() {
 		return nombre;
 	}
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
@@ -24,9 +30,13 @@ public class PersonaEjemplo {
 		return apellido;
 	}
 	public void setApellido(String apellido) {
+<<<<<<< HEAD
 
          this.apellido=apellido;
 
+=======
+		this.apellido = apellido;
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
 	}
 	public String getCarrera() {
 		return carrera;
@@ -34,12 +44,12 @@ public class PersonaEjemplo {
 	public void setCarrera(String carrera) {
 		this.carrera = carrera;
 	}
+<<<<<<< HEAD
+=======
+	
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
 	@Override
 	public String toString() {
-
 		return "PersonaEjemplo [nombre=" + nombre + ", apellido=" + apellido + ", carrera=" + carrera + "]";
 	}
-	
-	
 }
-
--- src/main/java/sv/gob/service/asignacion01/IVA18045Service.java
+++
@@ -1,9 +0,0 @@
-package sv.gob.service.asignacion01;
-
-import java.util.List;
-
-import sv.gob.models.PersonaEjemplo;
-
-public interface IVA18045Service {
-List<PersonaEjemplo> mostrar();
-}
