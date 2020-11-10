8bdad9ad6738ba53f13d43fa4d02313662089161
MH17015 <MH17015@ues.edu.sv>
2020-11-09 18:26:21 -0600

    Revert "Modificación 03"
    This reverts commit afe479167cd452f8cee778f674b557537c142de4.

--- src/main/java/sv/gob/models/PersonaEjemplo.java
+++ src/main/java/sv/gob/models/PersonaEjemplo.java
@@ -1,7 +1,8 @@
 package sv.gob.models;
 
 public class PersonaEjemplo {
-
+<<<<<<< HEAD
+<<<<<<< HEAD
 
     private String nombre;
     private String apellido;
@@ -11,6 +12,23 @@ public class PersonaEjemplo {
     	return nombre;	
     }   
 
+=======
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
+=======
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
+	private String nombre;
+	private String apellido;
+	private String carrera;
+	public String getNombre() {
+		return nombre;
+	}
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
+=======
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
@@ -18,7 +36,17 @@ public class PersonaEjemplo {
 		return apellido;
 	}
 	public void setApellido(String apellido) {
+<<<<<<< HEAD
+<<<<<<< HEAD
 
+         this.apellido=apellido;
+
+=======
+		this.apellido = apellido;
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
+=======
+		this.apellido = apellido;
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
 	}
 	public String getCarrera() {
 		return carrera;
@@ -26,7 +54,14 @@ public class PersonaEjemplo {
 	public void setCarrera(String carrera) {
 		this.carrera = carrera;
 	}
-
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+	
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
+=======
+	
+>>>>>>> parent of 8df8d02... Merge branch 'develop' into GD13021_asignacion1
 	@Override
 	public String toString() {
 		return "PersonaEjemplo [nombre=" + nombre + ", apellido=" + apellido + ", carrera=" + carrera + "]";
